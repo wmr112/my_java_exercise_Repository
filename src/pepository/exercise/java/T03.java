f7e9eeb78721abb093a450bcc065983bff1ae45a
root <wang1123142023@163.com>
2026-05-15 14:35:12 +0800

    aaa

--- src/pepository/exercise/java/T03.java
+++ src/pepository/exercise/java/T03.java
@@ -6,6 +6,12 @@ public class T03 {
         int a = (2);
         System.out.println(a++); // 2
         int b = (2);
-        System.out.println(b--);
+        System.out.println(b--); // 2
+        int c = (2);
+        c++;
+        System.out.println(c);
+        int d = (2);
+        d--;
+        System.out.println(d);
     }
 }
