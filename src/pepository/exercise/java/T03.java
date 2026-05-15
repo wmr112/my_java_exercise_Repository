7981c1148d6e00e4eb61b5149eb616f3a9cccca0
root <wang1123142023@163.com>
2026-05-15 14:20:03 +0800

    aaa

---
+++ src/pepository/exercise/java/T03.java
@@ -0,0 +1,11 @@
+package pepository.exercise.java;
+
+public class T03 {
+    public static void main(String[] args) {
+        //自增自减测试
+        int a = (2);
+        System.out.println(a++); // 2
+        int b = (2);
+        System.out.println(b--);
+    }
+}
