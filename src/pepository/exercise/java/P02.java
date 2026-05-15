fe6ebb2e25b4140a23bab21f4a552ef2ca914cd0
root <wang1123142023@163.com>
2026-05-15 13:52:11 +0800

    aaa

---
+++ src/pepository/exercise/java/P02.java
@@ -0,0 +1,12 @@
+package pepository.exercise.java;
+
+public class P02 {
+    public static void main(String[] args) {
+        //手机信息登记(变量&数据类型练习
+        String brand = ("小米17pro");
+        Double size = (6.3);
+        int ram = (16);
+        int price = (5599);
+        System.out.println ("手机品牌为"+brand+"\n屏幕尺寸为"+size+"\n手机内存为"+ram+"\n手机价格为"+price);
+    }
+}
